4f95686d1982b719b9ec4468067efa17e35fc3a1
imivan <imivan@e12f6628-ef46-11dd-88c7-29a3b14d5316>
2009-01-31 06:07:49 +0000

    Updated Character class to include a textures dictionary containing the images for each orientation

--- trunk/EverDarker/Character.cs
+++ trunk/EverDarker/Character.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace EverDarker
 {
     class Character
     {
-        public int xposition { get; set; }
-        public int yposition { get; set; }
-        public Orientations orientation { get; set; }
+        public Character()
+        {
+            this.xPosition = 50;
+            this.yPosition = 50;
+            this.textures = new Dictionary<Orientations, string>();
+            this.textures[Orientations.up] = "characterup.png";
+            this.textures[Orientations.left] = "characterleft.png";
+            this.textures[Orientations.right] = "characterright.png";
+            this.textures[Orientations.down] = "characterdown.png";
+        }
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
+        public Dictionary<Orientations, string> textures { get; set; }
     }
 }
 enum Orientations
