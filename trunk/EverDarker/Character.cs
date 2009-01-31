20d0dfda058ca8ee091c7c910429e3b1b257b73a
imivan <imivan@e12f6628-ef46-11dd-88c7-29a3b14d5316>
2009-01-31 23:28:54 +0000

    He Walks!

--- trunk/EverDarker/Character.cs
+++ trunk/EverDarker/Character.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
+using System.Timers;
 
 namespace EverDarker
 {
@@ -12,10 +13,19 @@ namespace EverDarker
     {
         public float RotationAngle = 0f;
         public Rectangle boundingBox;
+        public List<Texture2D> textures;
+        int texturesIndex = 1;
+        public bool walking = false;
+        public DateTime lastWalk;
 
         public void LoadContent(ContentManager theContentManager, string theAssetName, Viewport viewPort)
         {
+            textures = new List<Texture2D>();
             base.LoadContent(theContentManager, theAssetName);
+            textures.Add(this.spriteTexture);
+            textures.Add(theContentManager.Load<Texture2D>("Character-1-RightFoot"));
+            textures.Add(theContentManager.Load<Texture2D>("Character-1-LeftFoot"));
+
             this.Position = new Vector2(viewPort.Width / 2, viewPort.Height / 2);
             this.boundingBox = new Rectangle((int)this.Position.X, (int)this.Position.Y, this.spriteTexture.Width, this.spriteTexture.Height);
         }
@@ -25,6 +35,20 @@ namespace EverDarker
         {
             theSpriteBatch.Draw(spriteTexture, Position,
                 null, Color.White, RotationAngle, origin, Scale, SpriteEffects.None, 0);
+        }
+
+        public void Walk(SpriteBatch theSpriteBatch)
+        {
+            DateTime now = DateTime.Now;
+            if (now.Ticks > (this.lastWalk.Ticks + 2000000))
+            {
+                texturesIndex++;
+                if (texturesIndex > 2)
+                    texturesIndex = 1;
+                this.lastWalk = now;
+            }
+            theSpriteBatch.Draw(textures[texturesIndex], Position,
+                    null, Color.White, RotationAngle, origin, Scale, SpriteEffects.None, 0);
         }
     }
 }
