0cfa686d0188104b7acea6e1a7fc5401b2b02c5e
imivan <imivan@e12f6628-ef46-11dd-88c7-29a3b14d5316>
2009-01-31 05:32:12 +0000

    forgot Character.cs!

---
+++ trunk/EverDarker/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EverDarker
+{
+    class Character
+    {
+        public int xposition { get; set; }
+        public int yposition { get; set; }
+        public Orientations orientation { get; set; }
+    }
+}
+enum Orientations
+{
+    up,
+    down,
+    left,
+    right
+}
