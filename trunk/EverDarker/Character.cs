ebb825b3f28bdf3e8fc72ff0e0964a7f92b4b7cc
imivan <imivan@e12f6628-ef46-11dd-88c7-29a3b14d5316>
2009-01-31 17:23:21 +0000

    Added makeshift background scrolling

--- trunk/EverDarker/Character.cs
+++ trunk/EverDarker/Character.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 
 namespace EverDarker
 {
     class Character : Sprite
     {
-
+        public void LoadContent(ContentManager theContentManager, string theAssetName, Viewport viewPort)
+        {
+            base.LoadContent(theContentManager, theAssetName);
+            this.Position = new Vector2(viewPort.Width / 2, viewPort.Height / 2);
+        }
     }
 }
--- trunk/EverDarker/Sprite.cs
+++ trunk/EverDarker/Sprite.cs
@@ -10,22 +10,36 @@ namespace EverDarker
 {
     class Sprite
     {
+        #region Members
         //The current position of the Sprite
         public Vector2 Position = new Vector2(0, 0);
-
         //The texture object used when drawing the sprite
         private Texture2D spriteTexture;
+        //The size of the Sprite
+        public Rectangle Size;
+        //Used to size the Sprite up or down from the original image
+        public float Scale = 1.0f;
+        //For the center of the sprite
+        private Vector2 origin;
+        
+        #endregion Members
 
         //Load the texture for the sprite using the Content Pipeline
         public void LoadContent(ContentManager theContentManager, string theAssetName)
         {
+            //Load and Calc sprite size
             spriteTexture = theContentManager.Load<Texture2D>(theAssetName);
+            origin.X = spriteTexture.Width / 2;
+            origin.Y = spriteTexture.Height / 2;
+            Size = new Rectangle(0, 0, (int)(spriteTexture.Width * Scale), (int)(spriteTexture.Height * Scale));
         }
         
         //Draw the sprite to the screen 
         public void Draw(SpriteBatch theSpriteBatch)
         {
-            theSpriteBatch.Draw(spriteTexture, Position, Color.White);
+            theSpriteBatch.Draw(spriteTexture, Position, 
+                new Rectangle(0, 0, spriteTexture.Width, spriteTexture.Height), Color.White, 
+                0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0);
         }
     }
 }
